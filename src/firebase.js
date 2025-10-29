b26445c6b180d7def3637a70ee2ef3978e2f2c3f
Samien Munwar <munwarsamien@gmail.com>
2025-10-29 10:22:24 -0700

    Firebase fix

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzBAo2PgeO5ZGjTHhZ335U0F0tryPCJbE",
-  authDomain: "bby01-7d3e8.firebaseapp.com",
-  projectId: "bby01-7d3e8",
-  storageBucket: "bby01-7d3e8.firebasestorage.app",
-  messagingSenderId: "709025938649",
-  appId: "1:709025938649:web:569f14030351ac6ec652c8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
+export { db };
 export { auth };
