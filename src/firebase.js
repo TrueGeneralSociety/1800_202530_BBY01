7f3e0e84204cf7f6e1b0c5c02314bc3bb979f036
Samien Munwar <munwarsamien@gmail.com>
2025-11-03 18:06:04 -0800

    Added bootstrap library (because it's not rendering otherwise for some reason)

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
