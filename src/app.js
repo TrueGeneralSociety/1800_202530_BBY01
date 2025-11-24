e75328bd88c79c4dcef4256672e6fd2e8abbecd0
Alex Lee <alexclee7@gmail.com>
2025-11-24 11:34:06 -0800

    fixed issue with course load in dropbox

--- src/app.js
+++
@@ -1,6 +0,0 @@
-/*Purpose of this file - importing bootstrap */
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
-
-function sayHello() {}
-document.addEventListener("DOMContentLoaded", sayHello);
